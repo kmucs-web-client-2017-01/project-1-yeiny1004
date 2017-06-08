a4a885f77af5e136935aeb8a32a697cbe4e658d8
CHOI YE IN <yeiny1004@CHOIui-MacBook-Air.local>
2017-06-08 23:31:27 +0900

    project final

--- home_profile.js
+++ home_profile.js
@@ -27,12 +27,12 @@ function zoomInAndOut(string) {
   var currentWidth = image.width; 
   var currentHeight = image.height; 
   if (string == "in") { 
-    var width = parseInt(currentWidth * 1.1); //10%씩 넓이 증가 
-    var height = parseInt(currentHeight * 1.1); //10%씩 높이 증가 
+    var width = parseInt(currentWidth * 1.1); 
+    var height = parseInt(currentHeight * 1.1);  
   } 
   else if (string == "out") { 
-    var width = parseInt(currentWidth * 0.9); //10%씩 넓이 감소 
-    var height = parseInt(currentHeight * 0.9); //10%씩 높이 감소 
+    var width = parseInt(currentWidth * 0.9);  
+    var height = parseInt(currentHeight * 0.9);  
   } 
   image.width = width; 
   image.height = height; 
@@ -40,11 +40,11 @@ function zoomInAndOut(string) {
 
 $('.rabbit img').bind('click', function(){
         if(this.src == 'rabbit2.gif') this.src = 'rabbit1.gif';
-        else $(this).attr('src', 'rabbit2.gif'); // jQuery 로 src 속성값 바꿔보기
+        else $(this).attr('src', 'rabbit2.gif'); 
     });
  $('.css_test img').bind('click', function(){
         if(this.src == 'rabbit2.gif') this.src = 'rabbit1.gif';
-        else $(this).attr('src', 'rabbit2.gif'); // jQuery 로 src 속성값 바꿔보기
+        else $(this).attr('src', 'rabbit2.gif'); 
     });
 
 
